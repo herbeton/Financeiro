b59c7e12155d6cc9abbd414f3070d2ee1c90b0ed
Herbeton Bispo <Herbeton Bispo@HerbetonBispo>
2018-11-26 09:42:46 -0300

    Terminando o DAO das classes

---
+++ Financeiro/src/main/java/br/com/financeiro/dao/CidadeDAO.java
@@ -0,0 +1,7 @@
+package br.com.financeiro.dao;
+
+import br.com.financeiro.domain.Cidade;
+
+public class CidadeDAO extends GenericDAO<Cidade>{
+
+}
---
+++ Financeiro/src/main/java/br/com/financeiro/dao/VendaDAO.java
@@ -0,0 +1,7 @@
+package br.com.financeiro.dao;
+
+import br.com.financeiro.domain.Venda;
+
+public class VendaDAO extends GenericDAO<Venda>{
+
+}
