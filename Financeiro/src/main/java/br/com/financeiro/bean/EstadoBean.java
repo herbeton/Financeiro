ff2dcc2d2615033ed859ce94df503c711daee8dc
Herbeton Bispo <Herbeton Bispo@HerbetonBispo>
2018-11-26 16:18:08 -0300

    Exclusao

--- Financeiro/src/main/java/br/com/financeiro/bean/EstadoBean.java
+++ Financeiro/src/main/java/br/com/financeiro/bean/EstadoBean.java
@@ -7,6 +7,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
 
 import org.omnifaces.util.Messages;
 
@@ -40,6 +41,7 @@ public class EstadoBean implements Serializable {
 			estadoDAO.salvar(estado);
 
 			novo();
+			estados = estadoDAO.listar();
 
 			Messages.addGlobalInfo("Estado salvo com sucesso");
 		} catch (RuntimeException erro) {
@@ -55,6 +57,22 @@ public class EstadoBean implements Serializable {
 			setEstados(estadoDAO.listar());
 		} catch (RuntimeException erro) {
 			Messages.addGlobalError("Ocorreu um erro ao tentar listar os estados");
+			erro.printStackTrace();
+		}
+	}
+	
+	public void excluir(ActionEvent evento) {
+		try {
+			estado = (Estado) evento.getComponent().getAttributes().get("estadoSelecionado");
+
+			EstadoDAO estadoDAO = new EstadoDAO();
+			estadoDAO.excluir(estado);
+			
+			estados = estadoDAO.listar();
+
+			Messages.addGlobalInfo("Estado removido com sucesso");
+		} catch (RuntimeException erro) {
+			Messages.addFlashGlobalError("Ocorreu um erro ao tentar remover o estado");
 			erro.printStackTrace();
 		}
 	}
